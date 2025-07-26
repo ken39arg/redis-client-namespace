cad33090df65386eb7044a3c6bb14078d4ca1b34
Kensaku Araga <k_araga@ivry.jp>
2025-07-26 20:36:37 +0900

    initial commit by  bundle gem redis-client-namespace

---
+++ lib/redis/client/namespace/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Redis
+  module Client
+    module Namespace
+      VERSION = "0.1.0"
+    end
+  end
+end
---
+++ spec/redis/client/namespace_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Redis::Client::Namespace do
+  it "has a version number" do
+    expect(Redis::Client::Namespace::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
