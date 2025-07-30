34f1a6a62e5e9b43858749608806187fea0c5c09
Kensaku Araga <k_araga@ivry.jp>
2025-07-30 10:07:55 +0900

    refactor: improve test visibility and maintainability for command patterns
    - Standardize test output format to show input/output transformations clearly
    - Consolidate manual test patterns into a single data structure
    - Make it easier to review command coverage and add new test patterns
    - Align manual spec format with auto-generated spec for consistency

--- spec/redis_client/namespace/command_builder_auto_spec.rb
+++ spec/redis_client/namespace/command_builder_auto_spec.rb
@@ -2,11 +2,68 @@
 
 require "json"
 
-RSpec.describe RedisClient::Namespace do
-  let(:namespace) { "test" }
-  let(:builder) { described_class.new(namespace) }
+def make_test_case(namespace, cmd_name, arguments, multi_size: 1, non_optional: false)
+  inputs = cmd_name.split
+  outputs = inputs.dup
+  arguments.each do |arg|
+    next if non_optional && arg["optional"] && arg["type"] != "key"
 
+    input = [arg["token"]].compact
+
+    case arg["type"]
+    when "key"
+      keys = if arg["multiple"]
+               multi_size.times.with_index.map { |i| "#{arg["name"]}#{i + 1}" }
+             else
+               ["#{arg["name"]}1"]
+             end
+      output = input.dup
+      input += keys
+      output += keys.map { |k| "#{namespace}:#{k}" }
+    when "pattern"
+      patterns = if arg["multiple"]
+                   multi_size.times.with_index.map { |i| "#{arg["name"]}#{i + 1}*" }
+                 else
+                   ["#{arg["name"]}1*"]
+                 end
+      output = input.dup
+      input += patterns
+      output += patterns.map { |k| "#{namespace}:#{k}" }
+    when "string"
+      input << arg["name"]
+    when "integer"
+      input << (arg["name"] == "numkeys" ? multi_size : Random.rand(1..100))
+    when "double"
+      input << Random.rand(1.0..100.0).round(2)
+    when "unix-time"
+      input << Time.now.to_i
+    when "pure-token"
+      # input << arg["token"]
+    when "oneof"
+      selected = arg["arguments"].sample
+      input << selected["token"] if selected["token"]
+      case selected["type"]
+      when "integer"
+        input << rand(1..100)
+      when "unix-time"
+        input << Time.now.to_i
+      when "block"
+        input, output = make_test_case(namespace, selected["token"] || "", selected["arguments"] || [], multi_size: multi_size, non_optional: non_optional)
+      end
+    when "block"
+      # TODO: mutlipule
+      input, output = make_test_case(namespace, arg["token"] || "", arg["arguments"] || [], multi_size: multi_size, non_optional: non_optional)
+    end
+    inputs << input
+    outputs << (output || input)
+  end
+  [inputs.flatten, outputs.flatten.map(&:to_s)]
+end
+
+RSpec.describe RedisClient::Namespace do
   describe "all commands tests" do
+    namespace = "test"
+    builder = RedisClient::Namespace.new(namespace)
     # Load Redis commands
     # from: https://raw.githubusercontent.com/redis/docs/refs/heads/main/data/commands.json
     redis_commands = JSON.parse(File.read(File.join(__dir__, "commands.json")))
@@ -21,108 +78,58 @@ RSpec.describe RedisClient::Namespace do
       SORT SORT_RO
     ].freeze
 
-    def make_test_case(cmd_name, arguments, multi_size: 1, non_optional: false)
-      inputs = cmd_name.split
-      outputs = inputs.dup
-      arguments.each do |arg|
-        next if non_optional && arg["optional"] && arg["type"] != "key"
-
-        input = [arg["token"]].compact
-
-        case arg["type"]
-        when "key"
-          keys = if arg["multiple"]
-                   multi_size.times.with_index.map { |i| "#{arg["name"]}#{i + 1}" }
-                 else
-                   ["#{arg["name"]}1"]
-                 end
-          output = input.dup
-          input += keys
-          output += keys.map { |k| "#{namespace}:#{k}" }
-        when "pattern"
-          patterns = if arg["multiple"]
-                       multi_size.times.with_index.map { |i| "#{arg["name"]}#{i + 1}*" }
-                     else
-                       ["#{arg["name"]}1*"]
-                     end
-          output = input.dup
-          input += patterns
-          output += patterns.map { |k| "#{namespace}:#{k}" }
-        when "string"
-          input << arg["name"]
-        when "integer"
-          input << (arg["name"] == "numkeys" ? multi_size : Random.rand(1..100))
-        when "double"
-          input << Random.rand(1.0..100.0).round(2)
-        when "unix-time"
-          input << Time.now.to_i
-        when "pure-token"
-          # input << arg["token"]
-        when "oneof"
-          selected = arg["arguments"].sample
-          input << selected["token"] if selected["token"]
-          case selected["type"]
-          when "integer"
-            input << rand(1..100)
-          when "unix-time"
-            input << Time.now.to_i
-          when "block"
-            input, output = make_test_case(selected["token"] || "", selected["arguments"] || [], multi_size: multi_size, non_optional: non_optional)
-          end
-        when "block"
-          # TODO: mutlipule
-          input, output = make_test_case(arg["token"] || "", arg["arguments"] || [], multi_size: multi_size, non_optional: non_optional)
-        end
-        inputs << input
-        outputs << (output || input)
-      end
-      [inputs.flatten, outputs.flatten.map(&:to_s)]
-    end
-
     redis_commands.each do |cmd_name, cmd_info|
       # Skip commands that need manual testing
       next if manual_test_commands.include?(cmd_name.split.first)
 
       arguments = cmd_info["arguments"] || []
 
-      has_key = arguments.any? { |arg| ["key", "pattern"].include?(arg["type"]) }
+      arguments.any? { |arg| ["key", "pattern"].include?(arg["type"]) }
       has_multiple_key = arguments.any? { |arg| arg["type"] == "key" && arg["multiple"] }
       has_optional_key = arguments.any? { |arg| arg["type"] == "key" && arg["optional"] }
       has_optional_arg = arguments.any? { |arg| arg["type"] != "key" && arg["optional"] }
 
-      context "#{cmd_name} command" do
-        if has_key
-          it "should transform keys" do
-            inputs, outputs = make_test_case(cmd_name, arguments)
-            expect(builder.generate(inputs)).to eq(outputs)
-          end
+      testcases = []
 
-          if has_multiple_key
-            it "should handle multiple keys" do
-              inputs, outputs = make_test_case(cmd_name, arguments, multi_size: rand(2..5))
-              expect(builder.generate(inputs)).to eq(outputs)
-            end
-          end
+      inputs, outputs = make_test_case(namespace, cmd_name, arguments)
+      testcases << {
+        pattern: "basic",
+        inputs: inputs,
+        outputs: outputs
+      }
 
-          if has_optional_key
-            it "should handle optional keys" do
-              inputs, outputs = make_test_case(cmd_name, arguments, multi_size: 0)
-              expect(builder.generate(inputs)).to eq(outputs)
-            end
-          end
-        else
-          it "should not transform. no key arguments" do
-            inputs, outputs = make_test_case(cmd_name, arguments)
-            expect(builder.generate(inputs)).to eq(outputs)
-          end
-        end
+      if has_multiple_key
+        inputs, outputs = make_test_case(namespace, cmd_name, arguments, multi_size: rand(2..5))
+        testcases << {
+          pattern: "multi keys",
+          inputs: inputs,
+          outputs: outputs
+        }
+      end
 
-        if has_optional_arg
-          it "should handle optional arguments nothing" do
-            inputs, outputs = make_test_case(cmd_name, arguments, non_optional: true)
-            expect(builder.generate(inputs)).to eq(outputs)
-          end
+      if has_optional_key
+        inputs, outputs = make_test_case(namespace, cmd_name, arguments, multi_size: 0)
+        testcases << {
+          pattern: "no optional keys",
+          inputs: inputs,
+          outputs: outputs
+        }
+      end
 
+      if has_optional_arg
+        inputs, outputs = make_test_case(namespace, cmd_name, arguments, non_optional: true)
+        testcases << {
+          pattern: "no optional args",
+          inputs: inputs,
+          outputs: outputs
+        }
+      end
+
+      context cmd_name.to_s do
+        testcases.each do |t|
+          it "#{t[:pattern]}: [#{t[:inputs].map(&:to_s).join(" ")}] -> [#{t[:outputs].map(&:to_s).join(" ")}]" do
+            expect(builder.generate(t[:inputs])).to eq(t[:outputs])
+          end
         end
       end
     end
--- spec/redis_client/namespace/command_builder_manual_spec.rb
+++ spec/redis_client/namespace/command_builder_manual_spec.rb
@@ -14,146 +14,198 @@ RSpec.describe RedisClient::Namespace do
     #   HSCAN SSCAN ZSCAN
     #   SORT SORT_RO
     # ]
+    testcases = {
+      "SUBSCRIBE" => [
+        {
+          pattern: "basic",
+          inputs: %w[SUBSCRIBE channel1],
+          outputs: %w[SUBSCRIBE test:channel1]
+        },
+        {
+          pattern: "multi keys",
+          inputs: %w[SUBSCRIBE channel1 channel2 channel3],
+          outputs: %w[SUBSCRIBE test:channel1 test:channel2 test:channel3]
+        }
+      ],
+      "UNSUBSCRIBE" => [
+        {
+          pattern: "basic",
+          inputs: %w[UNSUBSCRIBE channel1],
+          outputs: %w[UNSUBSCRIBE test:channel1]
+        },
+        {
+          pattern: "multi keys",
+          inputs: %w[UNSUBSCRIBE channel1 channel2 channel3],
+          outputs: %w[UNSUBSCRIBE test:channel1 test:channel2 test:channel3]
+        }
+      ],
+      "PSUBSCRIBE" => [
+        {
+          pattern: "basic",
+          inputs: %w[PSUBSCRIBE pattern*],
+          outputs: %w[PSUBSCRIBE test:pattern*]
+        },
+        {
+          pattern: "multi patterns",
+          inputs: %w[PSUBSCRIBE pattern* test*],
+          outputs: %w[PSUBSCRIBE test:pattern* test:test*]
+        }
+      ],
+      "PUNSUBSCRIBE" => [
+        {
+          pattern: "basic",
+          inputs: %w[PUNSUBSCRIBE pattern*],
+          outputs: %w[PUNSUBSCRIBE test:pattern*]
+        },
+        {
+          pattern: "multi patterns",
+          inputs: %w[PUNSUBSCRIBE pattern* test*],
+          outputs: %w[PUNSUBSCRIBE test:pattern* test:test*]
+        }
+      ],
+      "PUBLISH" => [
+        {
+          pattern: "basic",
+          inputs: %w[PUBLISH channel message],
+          outputs: %w[PUBLISH test:channel message]
+        }
+      ],
+      "PUBSUB" => [
+        {
+          pattern: "CHANNELS with pattern",
+          inputs: %w[PUBSUB CHANNELS pattern*],
+          outputs: %w[PUBSUB CHANNELS test:pattern*]
+        },
+        {
+          pattern: "NUMSUB with multi channels",
+          inputs: %w[PUBSUB NUMSUB channel1 channel2 channel3],
+          outputs: %w[PUBSUB NUMSUB test:channel1 test:channel2 test:channel3]
+        },
+        {
+          pattern: "NUMPAT",
+          inputs: %w[PUBSUB NUMPAT],
+          outputs: %w[PUBSUB NUMPAT]
+        },
+        {
+          pattern: "SHARDNUMSUB with multi channels",
+          inputs: %w[PUBSUB SHARDNUMSUB shard1 shard2],
+          outputs: %w[PUBSUB SHARDNUMSUB test:shard1 test:shard2]
+        }
+      ],
+      "XREAD" => [
+        {
+          pattern: "with COUNT and multi streams",
+          inputs: %w[XREAD COUNT 10 STREAMS stream1 stream2 0-0 0-0],
+          outputs: %w[XREAD COUNT 10 STREAMS test:stream1 test:stream2 0-0 0-0]
+        }
+      ],
+      "XREADGROUP" => [
+        {
+          pattern: "basic with multi streams",
+          inputs: %w[XREADGROUP GROUP mygroup consumer1 STREAMS stream1 stream2 > >],
+          outputs: %w[XREADGROUP GROUP mygroup consumer1 STREAMS test:stream1 test:stream2 > >]
+        },
+        {
+          pattern: "with COUNT and BLOCK",
+          inputs: %w[XREADGROUP GROUP mygroup consumer1 COUNT 10 BLOCK 1000 STREAMS stream1 >],
+          outputs: %w[XREADGROUP GROUP mygroup consumer1 COUNT 10 BLOCK 1000 STREAMS test:stream1 >]
+        }
+      ],
+      "GEORADIUS" => [
+        {
+          pattern: "basic",
+          inputs: %w[GEORADIUS key 15 37 200 km],
+          outputs: %w[GEORADIUS test:key 15 37 200 km]
+        },
+        {
+          pattern: "with STORE",
+          inputs: %w[GEORADIUS key 15 37 200 km STORE dest],
+          outputs: %w[GEORADIUS test:key 15 37 200 km STORE test:dest]
+        }
+      ],
+      "GEORADIUSBYMEMBER" => [
+        {
+          pattern: "basic",
+          inputs: %w[GEORADIUSBYMEMBER key member 100 km],
+          outputs: %w[GEORADIUSBYMEMBER test:key member 100 km]
+        },
+        {
+          pattern: "with STORE",
+          inputs: %w[GEORADIUSBYMEMBER key member 100 km STORE dest],
+          outputs: %w[GEORADIUSBYMEMBER test:key member 100 km STORE test:dest]
+        },
+        {
+          pattern: "with STOREDIST",
+          inputs: %w[GEORADIUSBYMEMBER key member 100 km STOREDIST dest],
+          outputs: %w[GEORADIUSBYMEMBER test:key member 100 km STOREDIST test:dest]
+        }
+      ],
+      "MIGRATE" => [
+        {
+          pattern: "single key",
+          inputs: %w[MIGRATE 127.0.0.1 6379 key 0 1000],
+          outputs: %w[MIGRATE 127.0.0.1 6379 test:key 0 1000]
+        },
+        {
+          pattern: "with KEYS option",
+          inputs: ["MIGRATE", "127.0.0.1", "6379", "", "0", "1000", "KEYS", "key1", "key2", "key3"],
+          outputs: ["MIGRATE", "127.0.0.1", "6379", "", "0", "1000", "KEYS", "test:key1", "test:key2", "test:key3"]
+        }
+      ],
+      "HSCAN" => [
+        {
+          pattern: "basic",
+          inputs: %w[HSCAN hash 0],
+          outputs: %w[HSCAN test:hash 0]
+        },
+        {
+          pattern: "with MATCH",
+          inputs: %w[HSCAN hash 0 MATCH field*],
+          outputs: %w[HSCAN test:hash 0 MATCH field*]
+        }
+      ],
+      "SSCAN" => [
+        {
+          pattern: "with MATCH and COUNT",
+          inputs: %w[SSCAN set 0 MATCH member* COUNT 10],
+          outputs: %w[SSCAN test:set 0 MATCH member* COUNT 10]
+        }
+      ],
+      "ZSCAN" => [
+        {
+          pattern: "with MATCH",
+          inputs: %w[ZSCAN zset 0 MATCH member*],
+          outputs: %w[ZSCAN test:zset 0 MATCH member*]
+        }
+      ],
+      "SORT" => [
+        {
+          pattern: "with BY, GET, STORE",
+          inputs: %w[SORT list BY weight_* GET object_* STORE result],
+          outputs: %w[SORT test:list BY test:weight_* GET test:object_* STORE test:result]
+        },
+        {
+          pattern: "with GET # (no transformation)",
+          inputs: %w[SORT list GET #],
+          outputs: %w[SORT test:list GET #]
+        }
+      ],
+      "SORT_RO" => [
+        {
+          pattern: "with patterns",
+          inputs: %w[SORT_RO list BY weight_* GET object_*],
+          outputs: %w[SORT_RO test:list BY test:weight_* GET test:object_*]
+        }
+      ]
+    }
 
-    context "Pub/Sub commands" do
-      it "processes multiple channels for SUBSCRIBE command" do
-        result = builder.generate(["SUBSCRIBE", "channel1", "channel2", "channel3"])
-        expect(result).to eq(["SUBSCRIBE", "test:channel1", "test:channel2", "test:channel3"])
-      end
-
-      it "processes multiple channels for UNSUBSCRIBE command" do
-        result = builder.generate(["UNSUBSCRIBE", "channel1", "channel2"])
-        expect(result).to eq(["UNSUBSCRIBE", "test:channel1", "test:channel2"])
-      end
-
-      it "processes multiple patterns for PSUBSCRIBE command" do
-        result = builder.generate(["PSUBSCRIBE", "pattern*", "test*"])
-        expect(result).to eq(["PSUBSCRIBE", "test:pattern*", "test:test*"])
-      end
-
-      it "processes multiple patterns for PUNSUBSCRIBE command" do
-        result = builder.generate(["PUNSUBSCRIBE", "pattern*"])
-        expect(result).to eq(["PUNSUBSCRIBE", "test:pattern*"])
-      end
-
-      it "PUBLISH command applies namespace to channel name" do
-        result = builder.generate(["PUBLISH", "channel", "message"])
-        expect(result).to eq(["PUBLISH", "test:channel", "message"])
-      end
-
-      it "PUBSUB CHANNELS command" do
-        result = builder.generate(["PUBSUB", "CHANNELS", "pattern*"])
-        expect(result).to eq(["PUBSUB", "CHANNELS", "test:pattern*"])
-      end
-
-      it "PUBSUB NUMSUB command with multiple channels" do
-        result = builder.generate(["PUBSUB", "NUMSUB", "channel1", "channel2", "channel3"])
-        expect(result).to eq(["PUBSUB", "NUMSUB", "test:channel1", "test:channel2", "test:channel3"])
-      end
-
-      it "PUBSUB NUMPAT command" do
-        result = builder.generate(["PUBSUB", "NUMPAT"])
-        expect(result).to eq(["PUBSUB", "NUMPAT"])
-      end
-
-      it "PUBSUB SHARDNUMSUB command with multiple channels" do
-        result = builder.generate(["PUBSUB", "SHARDNUMSUB", "shard1", "shard2"])
-        expect(result).to eq(["PUBSUB", "SHARDNUMSUB", "test:shard1", "test:shard2"])
-      end
-    end
-
-    context "Stream commands" do
-      it "XREAD command" do
-        result = builder.generate(["XREAD", "COUNT", "10", "STREAMS", "stream1", "stream2", "0-0", "0-0"])
-        expect(result).to eq(["XREAD", "COUNT", "10", "STREAMS", "test:stream1", "test:stream2", "0-0", "0-0"])
-      end
-
-      it "XREADGROUP command applies namespace to stream keys" do
-        result = builder.generate(["XREADGROUP", "GROUP", "mygroup", "consumer1", "STREAMS", "stream1", "stream2", ">", ">"])
-        expect(result).to eq(["XREADGROUP", "GROUP", "mygroup", "consumer1", "STREAMS", "test:stream1", "test:stream2", ">", ">"])
-      end
-
-      it "XREADGROUP handles COUNT and BLOCK options" do
-        result = builder.generate(["XREADGROUP", "GROUP", "mygroup", "consumer1", "COUNT", "10", "BLOCK", "1000", "STREAMS", "stream1", ">"])
-        expect(result).to eq(["XREADGROUP", "GROUP", "mygroup", "consumer1", "COUNT", "10", "BLOCK", "1000", "STREAMS", "test:stream1", ">"])
-      end
-    end
-
-    context "Geo commands" do
-      it "GEORADIUS command" do
-        result = builder.generate(["GEORADIUS", "key", "15", "37", "200", "km"])
-        expect(result).to eq(["GEORADIUS", "test:key", "15", "37", "200", "km"])
-      end
-
-      it "processes GEORADIUS command with STORE option" do
-        result = builder.generate(["GEORADIUS", "key", "15", "37", "200", "km", "STORE", "dest"])
-        expect(result).to eq(["GEORADIUS", "test:key", "15", "37", "200", "km", "STORE", "test:dest"])
-      end
-
-      it "GEORADIUSBYMEMBER command applies namespace to key and member references" do
-        result = builder.generate(["GEORADIUSBYMEMBER", "key", "member", "100", "km"])
-        expect(result).to eq(["GEORADIUSBYMEMBER", "test:key", "member", "100", "km"])
-      end
-
-      it "GEORADIUSBYMEMBER handles STORE option" do
-        result = builder.generate(["GEORADIUSBYMEMBER", "key", "member", "100", "km", "STORE", "dest"])
-        expect(result).to eq(["GEORADIUSBYMEMBER", "test:key", "member", "100", "km", "STORE", "test:dest"])
-      end
-
-      it "GEORADIUSBYMEMBER handles STOREDIST option" do
-        result = builder.generate(["GEORADIUSBYMEMBER", "key", "member", "100", "km", "STOREDIST", "dest"])
-        expect(result).to eq(["GEORADIUSBYMEMBER", "test:key", "member", "100", "km", "STOREDIST", "test:dest"])
-      end
-    end
-
-    context "MIGRATE command" do
-      it "MIGRATE command with single key" do
-        result = builder.generate(["MIGRATE", "127.0.0.1", "6379", "key", "0", "1000"])
-        expect(result).to eq(["MIGRATE", "127.0.0.1", "6379", "test:key", "0", "1000"])
-      end
-
-      it "MIGRATE command with KEYS option" do
-        result = builder.generate(["MIGRATE", "127.0.0.1", "6379", "", "0", "1000", "KEYS", "key1", "key2", "key3"])
-        expect(result).to eq(["MIGRATE", "127.0.0.1", "6379", "", "0", "1000", "KEYS", "test:key1", "test:key2", "test:key3"])
-      end
-    end
-
-    context "SCAN family commands with special pattern handling" do
-      it "HSCAN command applies namespace to key but not to pattern" do
-        result = builder.generate(["HSCAN", "hash", "0", "MATCH", "field*"])
-        expect(result).to eq(["HSCAN", "test:hash", "0", "MATCH", "field*"])
-      end
-
-      it "HSCAN command without MATCH option" do
-        result = builder.generate(["HSCAN", "hash", "0"])
-        expect(result).to eq(["HSCAN", "test:hash", "0"])
-      end
-
-      it "SSCAN command applies namespace to key but not to pattern" do
-        result = builder.generate(["SSCAN", "set", "0", "MATCH", "member*", "COUNT", "10"])
-        expect(result).to eq(["SSCAN", "test:set", "0", "MATCH", "member*", "COUNT", "10"])
-      end
-
-      it "ZSCAN command applies namespace to key but not to pattern" do
-        result = builder.generate(["ZSCAN", "zset", "0", "MATCH", "member*"])
-        expect(result).to eq(["ZSCAN", "test:zset", "0", "MATCH", "member*"])
-      end
-    end
-
-    context "SORT commands with complex pattern handling" do
-      it "SORT command with BY, GET, STORE options" do
-        result = builder.generate(["SORT", "list", "BY", "weight_*", "GET", "object_*", "STORE", "result"])
-        expect(result).to eq(["SORT", "test:list", "BY", "test:weight_*", "GET", "test:object_*", "STORE", "test:result"])
-      end
-
-      it "SORT command with GET # (no transformation)" do
-        result = builder.generate(["SORT", "list", "GET", "#"])
-        expect(result).to eq(["SORT", "test:list", "GET", "#"])
-      end
-
-      it "SORT_RO command with patterns" do
-        result = builder.generate(["SORT_RO", "list", "BY", "weight_*", "GET", "object_*"])
-        expect(result).to eq(["SORT_RO", "test:list", "BY", "test:weight_*", "GET", "test:object_*"])
+    testcases.each do |cmd_name, cases|
+      context cmd_name do
+        cases.each do |t|
+          it "#{t[:pattern]}: [#{t[:inputs].map(&:to_s).join(" ")}] -> [#{t[:outputs].map(&:to_s).join(" ")}]" do
+            expect(builder.generate(t[:inputs])).to eq(t[:outputs])
+          end
+        end
       end
     end
   end
